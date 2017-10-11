a2323ec85f2b51913876a3a6501cc8b5c3628374
Stanley Su <stanleysu00@gmail.com>
2017-10-12 09:59:04 +1100

    altered getWinner function to include winning coordinates

--- js/script.js
+++ js/script.js
@@ -27,33 +27,40 @@ GAME.getPlayer = function() {
 GAME.addMove = function(pos) {
 	// if desired position on board is vacant
 	if (!this.board[pos]) {
-		// add the move
+		++this.totalTurns;
 		this.board[pos] = this.currTurn;
-		// alternate current turn
 		this.currTurn = this.currTurn === 'X' ? 'O' : 'X';
-		// update the board
 		BOARD.update(this.board);
+		// check if game has ended
+		if (this.getWinner() || this.totalTurns === 9) {
+			BOARD.endGame(this.board);
+		}
 	}
 }
 
-// returns the winner, else null
+// returns an array containing winning coordinates and winner, else null
 GAME.getWinner = function(b) {
 	// check rows, columns and diagonals connected to centre square
-	if (b[3] === b[4] && b[4] === b[5] ||
-		b[1] === b[4] && b[4] === b[7] ||
-		b[0] === b[4] && b[4] === b[8] ||
-		b[2] === b[4] && b[4] === b[6]) {
-		return b[4];
+	if (b[3] === b[4] && b[4] === b[5]) {
+		return [3, 4, 5, b[4]];
+	} else if (b[1] === b[4] && b[4] === b[7]) {
+		return [1, 4, 7, b[4]];
+	} else if (b[0] === b[4] && b[4] === b[8]) {
+		return [0, 4, 8, b[4]];
+	} else if (b[2] === b[4] && b[4] === b[6]) {
+		return [2, 4, 6, b[4]];
 	}
 	// check rows, columns and diagonals connected to top-left square
-	if (b[0] === b[1] && b[1] === b[2] ||
-		b[0] === b[3] && b[3] === b[6]) {
-		return b[0];
+	if (b[0] === b[1] && b[1] === b[2]) {
+		return [0, 1, 2, b[1]];
+	} else if (b[0] === b[3] && b[3] === b[6]) {
+		return [0, 3, 6, b[3]];
 	}
 	// check rows, columns and diagonals connected to bottom-right square
-	if (b[2] === b[5] && b[5] === b[8] ||
-		b[6] === b[7] && b[7] === b[8]) {
-		return b[8];
+	if (b[2] === b[5] && b[5] === b[8]) {
+		return [2, 5, 8, b[5]];
+	} else if (b[6] === b[7] && b[7] === b[8]) {
+		return [6, 7, 8, b[7];
 	}
 	return null;
 }
@@ -67,6 +74,11 @@ BOARD.update = function(b) {
 	for (let i = 0; i < b.length; ++i) {
 		this.array[i].textContent = b[i] || '';
 	}
+}
+
+// displays the end game message 
+BOARD.endGame = function(b) {
+	// find the winner
 }
 
 APP.init = function() {
