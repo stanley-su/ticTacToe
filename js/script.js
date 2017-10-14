c5a0a0df9c1630858eaba12b9725de45342f0f04
Stanley Su <stanleysu00@gmail.com>
2017-10-14 12:34:52 +1100

    added canvas functionality for representing game

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// appearance ideas
+// mosaic theme (easter island blocks etc.)
+// html canvas, x sprouts out from dot, circle grows out from dot
+// 9 cubes, winning row merges into single cube which flips out winner
+
 const GAME  	 = {}, // model
 	  BOARD 	 = {}, // view
 	  CONTROLLER = {}, // controller
@@ -8,8 +13,8 @@ const GAME  	 = {}, // model
 // CONTROLLER => GAME => BOARD
 
 CONTROLLER.init = function() {
-	document.getElementById('board').addEventListener('click', function(e) {
-		GAME.addMove(e.target.id);
+	document.getElementById('canvas').addEventListener('click', function(e) {
+		console.log(e.offsetX, e.offsetY);
 	});
 }
 
@@ -70,28 +75,59 @@ GAME.getWinner = function(b) {
 }
 
 BOARD.init = function() {
-	this.array = document.querySelectorAll('.box');
+	this.canvas = document.getElementById('canvas');
+	this.canvas.width = 300;
+	this.canvas.height = 300;
+	this.c = this.canvas.getContext('2d');
+	// initialise positions of centre of each box into array
+	this.array = [];
+	let topLeftCentre = {x: this.canvas.width / 6, y: this.canvas.height / 6};
+	for (let i = 0; i < 9; ++i) {
+		// store the current box's row and column
+		let row = Math.floor(i / 3);
+		let col = i % 3;
+		this.array.push({x: topLeftCentre.x + (row * this.canvas.width / 3),
+						 y: topLeftCentre.y + (col * this.canvas.height / 3)});
+	}
+	this.particles = [];
+}
+
+BOARD.drawBoard = function() {
+	this.c.beginPath();
+
+	this.c.moveTo(this.canvas.width / 3, 10);
+	this.c.lineTo(this.canvas.width / 3, this.canvas.height - 10);
+
+	this.c.moveTo(this.canvas.width / 3 * 2, 10);
+	this.c.lineTo(this.canvas.width / 3 * 2, this.canvas.height - 10);
+
+	this.c.moveTo(10, this.canvas.height / 3);
+	this.c.lineTo(this.canvas.width - 10, this.canvas.height / 3);
+
+	this.c.moveTo(10, this.canvas.height / 3 * 2);
+	this.c.lineTo(this.canvas.width - 10, this.canvas.height / 3 * 2);
+
+	this.c.lineCap = "round";
+	this.c.lineWidth = 4;
+	this.c.strokeStyle = '#aaa';
+	this.c.stroke();
 }
 
 // displays board to user
-BOARD.update = function(b) {
-	for (let i = 0; i < b.length; ++i) {
-		this.array[i].textContent = b[i] || '';
+BOARD.update = function() {
+	this.c.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	this.drawBoard();
+	for (let i = 0; i < this.array.length; ++i) {
+		this.c.beginPath();
+		this.c.arc(this.array[i].x, this.array[i].y, 1, Math.PI * 2, 0);
+		this.c.stroke();
+		this.c.closePath();
 	}
 }
 
 // displays the end game message
 BOARD.endGame = function(b, winner) {
-	if (winner) {
-		// only display the winning boxes
-		for (let i = 0; i < this.array.length; ++i) {
-			if (winner.coords.indexOf(i) === -1) {
-				this.array[i].textContent = '';
-			}
-		}
-	} else {
-		alert('draw');
-	}
+	
 }
 
 APP.init = function() {
@@ -100,4 +136,9 @@ APP.init = function() {
 	BOARD.init();
 }
 
+APP.update = function() {
+	BOARD.update();
+}
+
 APP.init();
+APP.update();
