0049eeb49fd39f607cf5f89bb558b8126b0c2996
Stanley Su <stanleysu00@gmail.com>
2017-10-15 14:01:49 +1100

    completed everything

--- js/script.js
+++ js/script.js
@@ -1,144 +1,218 @@
 "use strict";
 
-// appearance ideas
-// mosaic theme (easter island blocks etc.)
-// html canvas, x sprouts out from dot, circle grows out from dot
-// 9 cubes, winning row merges into single cube which flips out winner
+const APP = function() {
+	const canvas = document.getElementById('canvas');
+	const c = canvas.getContext('2d');
 
-const GAME  	 = {}, // model
-	  BOARD 	 = {}, // view
-	  CONTROLLER = {}, // controller
-	  APP 		 = {}; // everything!
+	canvas.width = 300;
+	canvas.height = 300;
 
-// CONTROLLER => GAME => BOARD
+	const CONTROLLER = function() {
+		canvas.addEventListener('click', function(e) {
+			// calculate row
+			let col = Math.floor(e.offsetX / (canvas.width / 3));
+			let row = Math.floor(e.offsetY / (canvas.height / 3));
+			let pos = row * 3 + col;
+			GAME.addMove(pos);
+		});
+	}();
 
-CONTROLLER.init = function() {
-	document.getElementById('canvas').addEventListener('click', function(e) {
-		console.log(e.offsetX, e.offsetY);
-	});
-}
+	const GAME = function() {
+		let currPlayer = 'cross';
+		let totalTurns = 0;
+		const winCombos = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6]
+		];
+		let board = new Array(9).fill(NaN);
 
-GAME.init = function() {
-	this.board = new Array(9).fill(NaN);
-	this.currTurn = 'X';
-	this.totalTurns = 0;
-}
-
-GAME.getPlayer = function() {
-	return this.currTurn;
-};
-
-// adds the current player's move into their desired position
-GAME.addMove = function(pos) {
-	// if desired position on board is vacant
-	if (!this.board[pos]) {
-		++this.totalTurns;
-		this.board[pos] = this.currTurn;
-		this.currTurn = this.currTurn === 'X' ? 'O' : 'X';
-		BOARD.update(this.board);
-
-		let winner = this.getWinner(this.board);
-		// check if game has ended
-		if (winner || this.totalTurns === 9) {
-			BOARD.endGame(this.board, winner);
-			this.init();
-			BOARD.init();
+		const getPlayer = function() {
+			return currPlayer;
 		}
-	}
-}
 
-// returns an array containing winning coordinates and winner, else null
-GAME.getWinner = function(b) {
-	// check rows, columns and diagonals connected to centre square
-	if (b[3] === b[4] && b[4] === b[5]) {
-		return {coords: [3, 4, 5], winner: b[4]};
-	} else if (b[1] === b[4] && b[4] === b[7]) {
-		return {coords: [1, 4, 7], winner: b[4]};
-	} else if (b[0] === b[4] && b[4] === b[8]) {
-		return {coords: [0, 4, 8], winner: b[4]};
-	} else if (b[2] === b[4] && b[4] === b[6]) {
-		return {coords: [2, 4, 6], winner: b[4]};
-	}
-	// check rows, columns and diagonals connected to top-left square
-	if (b[0] === b[1] && b[1] === b[2]) {
-		return {coords: [0, 1, 2], winner: b[1]};
-	} else if (b[0] === b[3] && b[3] === b[6]) {
-		return {coords: [0, 3, 6], winner: b[3]};
-	}
-	// check rows, columns and diagonals connected to bottom-right square
-	if (b[2] === b[5] && b[5] === b[8]) {
-		return {coords: [2, 5, 8], winner: b[5]};
-	} else if (b[6] === b[7] && b[7] === b[8]) {
-		return {coords: [6, 7, 8], winner: b[7]};
-	}
-	return null;
-}
+		const getWinner = function() {
+			// check the board for all winning combinations
+			for (let i = 0; i < winCombos.length; ++i) {
+				let w = winCombos[i];
+				// if all of the winning combo's positions on the board are equal
+				if (w.every((val, i, arr) => board[val] === board[arr[0]])) {
+					return w;
+				}
+			}
+			return null;
+		}
 
-BOARD.init = function() {
-	this.canvas = document.getElementById('canvas');
-	this.canvas.width = 300;
-	this.canvas.height = 300;
-	this.c = this.canvas.getContext('2d');
-	// initialise positions of centre of each box into array
-	this.array = [];
-	let topLeftCentre = {x: this.canvas.width / 6, y: this.canvas.height / 6};
-	for (let i = 0; i < 9; ++i) {
-		// store the current box's row and column
-		let row = Math.floor(i / 3);
-		let col = i % 3;
-		this.array.push({x: topLeftCentre.x + (row * this.canvas.width / 3),
-						 y: topLeftCentre.y + (col * this.canvas.height / 3)});
-	}
-	this.particles = [];
-}
+		const restart = function() {
+			board = board.map(val => NaN);
+			currPlayer = 'cross';
+			totalTurns = 0;
+		}
 
-BOARD.drawBoard = function() {
-	this.c.beginPath();
+		const addMove = function(pos) {
+			// if desired position on board is vacant
+			if (!board[pos]) {
+				// update game board
+				board[pos] = currPlayer;
+				// desired position on board's symbol map
+				let box = BOARD.symbols[pos];
+				// add current player to board's symbol map
+				box.symbol = new BOARD[currPlayer](box.x, box.y, canvas.width / 10, 3);
+				currPlayer = (currPlayer === 'cross' ? 'nought' : 'cross');
+				totalTurns += 1;
 
-	this.c.moveTo(this.canvas.width / 3, 10);
-	this.c.lineTo(this.canvas.width / 3, this.canvas.height - 10);
+				let winner = getWinner();
+				// check if game has ended
+				if (winner || totalTurns === 9) {
+					BOARD.endGame(winner);
+					restart();
+				}
+			}
+		}
 
-	this.c.moveTo(this.canvas.width / 3 * 2, 10);
-	this.c.lineTo(this.canvas.width / 3 * 2, this.canvas.height - 10);
+		return { board, getPlayer, getWinner, addMove };
+	}();
 
-	this.c.moveTo(10, this.canvas.height / 3);
-	this.c.lineTo(this.canvas.width - 10, this.canvas.height / 3);
+	const BOARD = function() {
+		const symbols = [];
+		let topLeftCentre = {x: canvas.width / 6, y: canvas.height / 6};
+		for (let i = 0; i < 9; ++i) {
+			// store the current box's row and column
+			let row = Math.floor(i / 3);
+			let col = i % 3;
+			symbols.push({x: topLeftCentre.x + (col * canvas.width / 3),
+							 y: topLeftCentre.y + (row * canvas.height / 3)});
+		}
 
-	this.c.moveTo(10, this.canvas.height / 3 * 2);
-	this.c.lineTo(this.canvas.width - 10, this.canvas.height / 3 * 2);
+		const restart = function() {
+			// delete all of the symbols
+			for (let i = 0; i < symbols.length; ++i) {
+				if (symbols[i].symbol) {
+					setTimeout(function() {
+						delete symbols[i].symbol;
+					}, 100);
+				}
+			}
+		}
 
-	this.c.lineCap = "round";
-	this.c.lineWidth = 4;
-	this.c.strokeStyle = '#aaa';
-	this.c.stroke();
-}
+		const nought = function(x, y, d, r) {
+			this.originX = x;
+			this.originY = y;
+			this.x = x;
+			this.y = y;
+			this.d = d;
+			this.r = r;
+			this.a = 0;
+			this.update = function() {
+				this.a += 0.05;
+				this.x = this.originX + this.d * Math.cos(this.a);
+				this.y = this.originY + this.d * Math.sin(this.a);
+				this.draw();
+			}
+			this.draw = function() {
+				c.beginPath();
+				c.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+				c.fillStyle = 'white';
+				c.fill();
+			}
+		}
 
-// displays board to user
-BOARD.update = function() {
-	this.c.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	this.drawBoard();
-	for (let i = 0; i < this.array.length; ++i) {
-		this.c.beginPath();
-		this.c.arc(this.array[i].x, this.array[i].y, 1, Math.PI * 2, 0);
-		this.c.stroke();
-		this.c.closePath();
-	}
-}
+		const cross = function(x, y, d, r) {
+			this.tl = { x: x - d, y: y - d };
+			this.tr = { x: x + d, y: y - d};
+			this.bl = { x: x - d, y: y + d};
+			this.br = { x: x + d, y: y + d};
+			// first orb
+			this.x1 = this.tl.x;
+			this.y1 = this.tl.y;
+			// second orb
+			this.x2 = this.bl.x;
+			this.y2 = this.bl.y;
+			// velocity
+			this.v = 1;
+			this.r = r;
+			this.s1 = 1;
+			this.s2 = 1;
+			this.update = function() {
+				// variables that decide velocity's positivity
+				if (this.x1 > this.br.x || this.x1 < this.tl.x) {
+					this.s1 *= -1;
+				}
+				if (this.x2 > this.tr.x || this.x2 < this.bl.x) {
+					this.s2 *= -1;
+				}
+				this.x1 += this.v * this.s1;
+				this.y1 += this.v * this.s1;
+				this.x2 += this.v * this.s2;
+				this.y2 -= this.v * this.s2;
+				this.draw();
+			}
+			this.draw = function() {
+				c.beginPath();
+				c.arc(this.x1, this.y1, this.r, 0, Math.PI * 2);
+				c.arc(this.x2, this.y2, this.r, 0, Math.PI * 2);
+				c.fillStyle = 'white';
+				c.fill();
+			}
+		}
 
-// displays the end game message
-BOARD.endGame = function(b, winner) {
-	
-}
+		const drawBoard = function() {
+			c.beginPath();
+			c.moveTo(canvas.width / 3, 10);
+			c.lineTo(canvas.width / 3, canvas.height - 10);
+			c.moveTo(canvas.width / 3 * 2, 10);
+			c.lineTo(canvas.width / 3 * 2, canvas.height - 10);
+			c.moveTo(10, canvas.height / 3);
+			c.lineTo(canvas.width - 10, canvas.height / 3);
+			c.moveTo(10, canvas.height / 3 * 2);
+			c.lineTo(canvas.width - 10, canvas.height / 3 * 2);
+			c.lineCap = "round";
+			c.lineWidth = 1;
+			c.strokeStyle = '#aaa';
+			c.stroke();
+		}
 
-APP.init = function() {
-	CONTROLLER.init();
-	GAME.init();
-	BOARD.init();
-}
+		const endGame = function(w) {
+			if (w) {
+				for (let i = 0; i < symbols.length; ++i) {
+					if (w.indexOf(i) === -1) {
+						delete symbols[i].symbol;
+					}
+				}
+				setTimeout(restart, 1000);
+			} else {
+				restart();
+			}
+		}
 
-APP.update = function() {
-	BOARD.update();
-}
+		const update = function() {
+			requestAnimationFrame(update);
+			c.fillStyle = 'rgba(0,0,0,0.1)';
+			c.fillRect(0, 0, canvas.width, canvas.height);
+			drawBoard();
+			for (let i = 0; i < symbols.length; ++i) {
+				if (symbols[i].symbol) {
+					for (let n = 0; n < 3; ++n) {
+						symbols[i].symbol.update();
+					}
+				}
+			}
+			
+		}		
 
-APP.init();
+		return { symbols, nought, cross, update, endGame };
+	}();
+
+	const update = function() {
+		BOARD.update();
+	};
+
+	return { update };
+}();
+
 APP.update();
